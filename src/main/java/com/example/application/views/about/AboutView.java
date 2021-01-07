fcebdf0cdde08f9a605786eb82ce4a6b0eb64df5
Galal Ahmed <islam.galal212@gmail.com>
2021-01-07 19:37:17 +0100

    Show about view at start

--- src/main/java/com/example/application/views/about/AboutView.java
+++ src/main/java/com/example/application/views/about/AboutView.java
@@ -6,6 +6,7 @@ import com.vaadin.flow.component.Text;
 import com.vaadin.flow.component.html.Div;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
+import com.vaadin.flow.router.RouteAlias;
 import org.apache.commons.mail.EmailException;
 
 import java.io.IOException;
@@ -13,6 +14,7 @@ import java.util.Arrays;
 
 @Route(value = "about", layout = MainView.class)
 @PageTitle("About")
+@RouteAlias(value = "", layout = MainView.class)
 public class AboutView extends Div {
 
     public AboutView() {
